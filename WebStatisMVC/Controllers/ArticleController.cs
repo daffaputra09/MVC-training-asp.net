63e5343ba78a973d5a1985da32057ff689e68a69
daffaputra09 <daffaputra863@gmail.com>
2023-05-15 13:50:53 +0700

    firs commit

---
+++ WebStatisMVC/Controllers/ArticleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebStatisMVC.Models;
+
+namespace WebStatisMVC.Controllers
+{
+    public class ArticleController : Controller
+    {   
+        public IActionResult Article()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
